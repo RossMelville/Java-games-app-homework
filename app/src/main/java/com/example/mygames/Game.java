8f92659ed2de5a6815417abaf61cff49537c7016
Ross Melville <rossmelville@hotmail.com>
2017-08-09 16:36:20 +0100

    Initial commit. Added a Game class and a GameTest class. Constructor method added to Game class.

---
+++ app/src/main/java/com/example/mygames/Game.java
@@ -0,0 +1,21 @@
+package com.example.mygames;
+
+/**
+ * Created by rossmelville on 09/08/2017.
+ */
+
+public class Game {
+
+    private int ranking;
+    private String title;
+    private String format;
+
+    public Game(int ranking, String title, String format) {
+        this.ranking = ranking;
+        this.title = title;
+        this.format = format;
+    }
+
+
+
+}
---
+++ app/src/test/java/com/example/mygames/GameTest.java
@@ -0,0 +1,22 @@
+package com.example.mygames;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by rossmelville on 09/08/2017.
+ */
+
+public class GameTest {
+
+    Game game;
+
+    @Before
+    public void before() {
+        game = new Game(1, "Super Mario World", "SNES");
+    }
+
+    @Test
+    
+
+}
